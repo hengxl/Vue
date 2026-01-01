d7d98649dd3b7d6e3d941385496ed34c4b2fa13e
hxl <hxl@git.com>
2026-01-01 18:54:14 +0800

    [2026/1/1]Props传递参数与泛型限制

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -12,7 +12,14 @@
     <!-- 使用 Watch 组件 -->
     <!-- <Watch/> -->
     <!-- 使用 WatchEffect 组件 -->
-    <WatchEffect/>
+    <!-- <WatchEffect/> -->
+    <!-- 使用 Props 组件 -->
+    <!-- stu-list="students" 和 :stu-list="students" 的区别是
+      前者是将 students 作为字符串传递给 Props 组件的 stu-list 属性
+      后者是将 students 作为变量传递给 Props 组件的 stu-list 属性 
+    -->
+    <Props a="哈哈" :stuList="students"/>
+    <!-- <Props a="哈哈" :stuList111="students"/> --> <!-- 测试默认值 -->
 </template>
 
 <!-- 写 TS 脚本 -->
@@ -43,6 +50,7 @@
 <script lang="ts" setup name="App">
 
     // 引入 Person 组件
+    import { reactive } from 'vue';
     import Basic1 from './components/01-Basics.vue'
     // 引入 Car 组件
     import Basic2 from './components/02-Basic.vue'
@@ -52,7 +60,17 @@
     import Watch from './components/04-Watch.vue';
     // 引入 WatchEffect 组件
     import WatchEffect from './components/05-WatchEffect.vue';
+    // 引入 Prop 组件
+    import Props from './components/07-Props.vue';
 
+    import { type StudentList } from './types';
+
+    // 定义学生列表数据
+    let students = reactive<StudentList>([
+        { id: 1, name: '张三', age: 18 },
+        { id: 2, name: '李四', age: 19 },
+        { id: 3, name: '王五', age: 20 }
+    ]);
 </script>
 
 <!-- 写 CSS 样式 -->
---
+++ Vue/src/components/07-Props.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="props">
+        <div v-for="stu in stuList" :key="stu.id">
+            学生姓名：{{ stu.name }}，学生年龄：{{ stu.age }}
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup name="Props-HXL">
+import { type StudentList } from '../types';
+
+// 接受stuList + 限制类型（StudentList） + 限制必要性（?） + 指定默认值
+withDefaults(defineProps<{ stuList?: StudentList }>(), {
+    stuList: () => [{ id: 0, name: '默认名', age: 0 }]
+});
+</script>
+
+<style scoped>
+    .props {
+        margin: center;
+        padding: 10px;
+        border: 2px solid #42b983;
+        border-radius: 5px;
+        width: 300px;
+        text-align: center;
+
+    }
+</style>
