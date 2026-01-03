37ee373a44ba07303c87ce4155c473da76152e29
hxl <hxl@git.com>
2026-01-03 22:36:17 +0800

    [2026/1/3]组合式

--- Vue-Pinia/src/stores/counter.ts
+++ Vue-Pinia/src/stores/counter.ts
@@ -1,26 +1,48 @@
 // 从 pinia 中引入 defineStore 函数
 import { defineStore } from 'pinia'
 
+import { ref } from 'vue'
+
 // 使用 defineStore 定义一个 Store
 // 第一个参数 'counter' 是 Store 的唯一 ID，Pinia 用它来连接 DevTools
 // 第二个参数是一个选项对象
-export const useCounterStore = defineStore('counter', {
-  // state 是一个函数，必须返回一个对象，用于存储状态
-  state: () => ({
-    count: 0,       // 当前的总和
-    step: 1,        // 每次加减的步长
-  }),
+// export const useCounterStore = defineStore('counter', {
+//   // state 是一个函数，必须返回一个对象，用于存储状态
+//   state: () => ({
+//     count: 0,       // 当前的总和
+//     step: 1,        // 每次加减的步长
+//   }),
 
-  // actions 是用来修改 state 的方法
-  actions: {
-    // 增加 count 的方法
-    increment() {
-      // 在 actions 中，你可以通过 this 直接访问 state 中的属性
-      this.count += this.step
-    },
-    // 减少 count 的方法
-    decrement() {
-      this.count -= this.step
-    },
-  },
+//   // actions 是用来修改 state 的方法
+//   actions: {
+//     // 增加 count 的方法
+//     increment() {
+//       // 在 actions 中，你可以通过 this 直接访问 state 中的属性
+//       this.count += this.step
+//     },
+//     // 减少 count 的方法
+//     decrement() {
+//       this.count -= this.step
+//     },
+//   },
+// })
+
+// ------------------ 组合式 ------------------------
+export const useCounterStore = defineStore('counter', () => {
+  // 1.定义 state：使用 ref 创建响应式状态
+  const count = ref(0)
+  const step = ref(1)
+
+  // 2.定义 actions：直接定义为普通函数
+  function increment() {
+    // 在函数中直接操作 ref 的 .value
+    count.value += step.value
+  }
+
+  function decrement() {
+    count.value -= step.value
+  }
+
+  // 3.返回所有需要在外部访问的状态和方法
+  return { count, step, increment, decrement}
 })
