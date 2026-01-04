e210ac66d9122fe50503d8640557c4952cbc5790
hengxiaoliang <hengxl@vvmeeting.com>
2026-01-04 18:31:02 +0800

    [2026/1/4]Vue3之父子组件准备

--- Vue-Component-Communication/src/pages/01_props/Child.vue
+++ Vue-Component-Communication/src/pages/01_props/Child.vue
@@ -1,5 +1,7 @@
 <template>
-    
+    <div class="child">
+        <h3>子组件</h3>
+    </div>
 </template>
 
 <script lang="ts" setup name="child-props">
@@ -8,5 +10,10 @@
 </script>
 
 <style scoped>
-
+    .child {
+        background-color: skyblue;
+        padding: 10px;
+        box-shadow: 0 0 10px black;
+        border-radius: 10px;
+    }
 </style>
--- Vue-Component-Communication/src/pages/01_props/Father.vue
+++ Vue-Component-Communication/src/pages/01_props/Father.vue
@@ -1,11 +1,26 @@
 <template>
-    
+    <div class="father">
+        <h3>父组件</h3>
+        <Child/>
+    </div>
 </template>
 
 <script lang="ts" setup name="father-props">
+    import Child from './Child.vue';
 
 </script>
 
 <style scoped>
+    .father{
+        /* 居中 */
+        /* 1. 设置一个宽度 */
+        width: 900px;
+        /* 2. 设置左右外边距为 auto，浏览器会自动平分剩余空间 */
+        margin: 0 auto;
 
+        
+        background-color: rgb(165, 164, 164);
+        padding: 20px;
+        border-radius: 10px;
+    }
 </style>
