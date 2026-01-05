8c7f6e5efce3cc7ec9efef6b3f186f36573a4926
hengxiaoliang <hengxl@vvmeeting.com>
2026-01-05 18:30:56 +0800

    [2026/1/5]作用域插槽

--- Vue-Component-Communication/src/pages/09_slot/Father.vue
+++ Vue-Component-Communication/src/pages/09_slot/Father.vue
@@ -1,13 +1,57 @@
 <template>
-    <div>
+    <div class="father">
+        <div class="game-container">
+                    <Game>
+            <!-- 将名为 gameSlot 的插槽内容赋值给 sonData -->
+            <template v-slot:gameSlot="sonData">
+                <!-- 无序列表 -->
+                <ul>
+                    <li v-for="item in sonData.games" :key="item.id">{{ item.name }}</li>
+                </ul>
+            </template>
+        </Game>
 
+        <Game>
+            <!-- 将名为 gameSlot 的插槽内容结构到 games 数据 -->
+            <template v-slot:gameSlot="{ games }">
+                <!-- 有序列表 -->
+                <ol>
+                    <li v-for="item in games" :key="item.id">{{ item.name }}</li>
+                </ol>
+            </template>
+        </Game>
+
+        <Game>
+            <!-- 将名为 gameSlot 的插槽内容结构到 games 数据 -->
+            <template #gameSlot="{ games }">
+                <h3 v-for="item in games" :key="item.id">{{ item.name }}</h3>
+            </template>
+        </Game>
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-
+    import Game from './Game.vue'
 </script>
 
 <style scoped>
+    .father{
+        /* 居中 */
+        /* 1. 设置一个宽度 */
+        width: 900px;
+        /* 2. 设置左右外边距为 auto，浏览器会自动平分剩余空间 */
+        margin: 0 auto;
 
+        background-color: rgb(165, 164, 164);
+        padding: 20px;
+        border-radius: 10px;
+    }
+
+    .game-container{
+        /* 设置浮动 */
+        display: flex;
+        /* 子元素均分开来 */
+        justify-content: space-evenly;
+    }
 </style>
---
+++ Vue-Component-Communication/src/pages/09_slot/Game.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="game">
+        <h2>游戏</h2>
+        <slot name="gameSlot" :games="gameList"></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue'
+    
+    // 数据定义在子里
+    const gameList = ref([{
+        id: 1,
+        name: '王者荣耀'
+    }, {
+        id: 2,
+        name: '原神'
+    }, {
+        id: 3,
+        name: '和平精英'
+    }, {
+        id: 4,
+        name: '使命召唤16'
+    }])
+</script>
+
+<style scoped>
+    .game{
+        width: 200px;
+        background-color: skyblue;
+        border-radius: 10px;
+        box-shadow: 0 0 10px;
+        text-align: center;
+    }
+</style>
